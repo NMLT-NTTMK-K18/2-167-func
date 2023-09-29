661c8cf82b911cea9c2afdf5dda7e4a1dc7835ed
truonghoangkhiem <truongkhiemhoang@gmail.com>
2023-09-29 22:17:22 +0700

    Cap nhat

--- Bai092/Source.cpp
+++ Bai092/Source.cpp
@@ -1,30 +1,38 @@
 #include<iostream>
 using namespace std;
+void Nhap(float&, int&);
+float Xuly(float&, int&);
 
 int main()
 {
-	int x;
-	cout << "Nhap x: ";
-	cin >> x;
-
+	float x;
 	int n;
-	cout << "Nhap n: ";
-	cin >> n;
-
-	float s = 1 - x;
-	float t = x;
+	Nhap(x, n);
+	cout << "s = " << Xuly(x, n);
+	return 0;
+}
+void Nhap(float& xx, int& nn)
+{
+	cout << "Nhap x = ";
+	cin >> xx;
+	cout << "Nhap n =";
+	cin >> nn;
+}
+float Xuly(float& xx, int& nn)
+{
+	float s = 1 - xx;
+	float t = xx;
 	int m = 1;
 	int i = 3;
 	int dau = 1;
 
-	while (i <= 2 * n + 1)
+	while (i <= 2 * nn + 1)
 	{
-		t = t * x * x;
+		t = t * xx * xx;
 		m = m * i * (i - 1);
 		s = s + dau * t / m;
 		i = i + 2;
 		dau = -dau;
 	}
-	cout << "s = " << s;
-	return 0;
+	return s;
 }
